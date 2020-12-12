c53e53df91550ee5ed4b12a296310fff80db8ecb
Anantha Krishnan <anantha.krishnan@ibsplc.com>
2020-12-12 22:39:05 +0530

    fix

--- container/bootstrap.js
+++ container/bootstrap.js
@@ -2,19 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom";
 import App from "./App.jsx"
 
-
-const mount = (el) =>{
-    ReactDOM.render(
-    <App></App> ,el)
-}
-
-if( process.env.NODE_ENV === 'development')
-{
-  const devRoot =  document.querySelector('#_marketing_dev_root');
- if(devRoot)
- {
-     mount(devRoot);
- }
-}
-
-export {mount};
+ReactDOM.render(<App/> ,document.querySelector('#_container_root'));
