20a56dc3d0cdff87aff7055887a5d547b5b38def
Anantha Krishnan <anantha.krishnan@ibsplc.com>
2020-12-13 12:45:36 +0530

    added header

--- container/src/App.jsx
+++ container/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp.jsx"
+import Header from "./components/Header.jsx"
+import { BrowserRouter } from "react-router-dom";
 
 
 export default () => {
   return (
+   <BrowserRouter>
     <div>
-   Hi there!!
-   <hr/>
+   <Header/>
    <MarketingApp/>
     </div>
+    </BrowserRouter>
+  
   );
 };
